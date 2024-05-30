9a82ee43ef5e857e28b9f7159c6ecc1cb6cb850a
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2024-05-30 03:29:31 +0200

    Adds an API endpoint to retrieve all of the meals from the database

---
+++ main.py
@@ -0,0 +1,38 @@
+# main.py
+from fastapi import FastAPI
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.future import select
+
+app = FastAPI()
+
+DATABASE_URL = "sqlite+aiosqlite:///./sqlite/meals.db"
+
+# Create database engine and session
+engine = create_async_engine(DATABASE_URL, echo=True)
+async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+metadata = MetaData()
+meals_table = Table(
+    "meals", metadata, autoload_with=create_engine("sqlite:///./sqlite/meals.db")
+)
+
+
+# http://localhost:5000/api/meals
+@app.get("/api/meals")
+async def get_meals():
+    async with async_session() as session:
+        async with session.begin():
+            result = await session.execute(select(meals_table))
+            meals = result.fetchall()
+            return [
+                {
+                    "id": meal.id,
+                    "name": meal.name,
+                    "description": meal.description,
+                    "price": meal.price,
+                    "path_to_photo": meal.path_to_photo,
+                }
+                for meal in meals
+            ]
